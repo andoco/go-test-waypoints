2ed8b9ee7a262c48bdb3b4ec374ffee90b609caa
Andrew O'Connor <andrew.oconnor@gmail.com>
2016-01-26 17:45:29 +0000

    Simulating waypoints in a loop

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"bitbucket.org/andoco/go-test-waypoints/waypoints"
 	log "github.com/Sirupsen/logrus"
@@ -10,8 +13,29 @@ import (
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
+
+	waypoints.SetOutput(os.Stdout)
+}
+
+func simulateWaypoints(c <-chan struct{}, wg sync.WaitGroup) {
+	log.Info("Simulating waypoints")
+
+	for i := 0; ; i++ {
+		waypoints.Visit(fmt.Sprintf("test-waypoint-%d", i))
+		time.Sleep(1 * time.Second)
+	}
+
+	log.Info("Finished simulating waypoints")
+	wg.Done()
 }
 
 func main() {
-	waypoints.Visit("test-waypoint")
+	var wg sync.WaitGroup
+
+	c := make(chan struct{})
+
+	wg.Add(1)
+	go simulateWaypoints(c, wg)
+
+	wg.Wait()
 }
